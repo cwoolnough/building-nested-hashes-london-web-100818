d865b909f70eaa2711ae166214f6dc196a8c26d0
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-24 12:30:35 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,15 +2,16 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague";
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague";
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        
         },
       :hero_friends => []
    }, 
